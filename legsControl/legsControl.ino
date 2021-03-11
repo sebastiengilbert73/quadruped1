0fd719c57c15101a14befa6a72a9870c101ac35d
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2021-03-10 19:41:28 -0500

    legsControl.ino: Crouch -> Sphinx

--- legsControl/legsControl.ino
+++ legsControl/legsControl.ino
@@ -20,18 +20,18 @@ uint8_t backLeftShoulderNdx = 12;
 uint8_t backLeftElbowNdx = 13;
 uint8_t backLeftWristNdx = 14;
 
-int frontLeftShoulderCrouch = 85;
-int frontRightShoulderCrouch = 110;
-int frontLeftElbowCrouch = 0;
-int frontRightElbowCrouch = 140;
-int frontLeftWristCrouch = 80;
-int frontRightWristCrouch = 130;
-int backLeftShoulderCrouch = 110;
-int backRightShoulderCrouch = 105;
-int backLeftElbowCrouch = 90;
-int backRightElbowCrouch = 120;
-int backLeftWristCrouch = 80;
-int backRightWristCrouch = 130;
+int frontLeftShoulderSphinx = 85;
+int frontRightShoulderSphinx = 110;
+int frontLeftElbowSphinx = 0;
+int frontRightElbowSphinx = 140;
+int frontLeftWristSphinx = 80;
+int frontRightWristSphinx = 130;
+int backLeftShoulderSphinx = 110;
+int backRightShoulderSphinx = 105;
+int backLeftElbowSphinx = 90;
+int backRightElbowSphinx = 120;
+int backLeftWristSphinx = 80;
+int backRightWristSphinx = 130;
 
 
 Adafruit_PWMServoDriver servoDriver = Adafruit_PWMServoDriver();
@@ -46,13 +46,13 @@ void setup()
 
   
 
-  Crouch();
+  Sphinx();
   delay(2000);
   Stand();
   delay(2000);
   //StretchWrists();
   //delay(2000);
-  Step(10);
+  //Step(10);
 }
 
 void loop()
@@ -61,27 +61,27 @@ void loop()
   
 }
 
-void Crouch()
+void Sphinx()
 {
   Serial.println("Crouch()");
   int delta = 300;
 
-  servoDriver.setPWM(frontLeftShoulderNdx, 0, map(frontLeftShoulderCrouch, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontRightShoulderNdx, 0, map(frontRightShoulderCrouch, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backLeftShoulderNdx, 0, map(backLeftShoulderCrouch, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backRightShoulderNdx, 0, map(backRightShoulderCrouch, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontLeftShoulderNdx, 0, map(frontLeftShoulderSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontRightShoulderNdx, 0, map(frontRightShoulderSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backLeftShoulderNdx, 0, map(backLeftShoulderSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backRightShoulderNdx, 0, map(backRightShoulderSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   delay(delta);
   
-  servoDriver.setPWM(frontLeftElbowNdx, 0, map(frontLeftElbowCrouch, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontRightElbowNdx, 0, map(frontRightElbowCrouch, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backLeftElbowNdx, 0, map(backLeftElbowCrouch, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backRightElbowNdx, 0, map(backRightElbowCrouch, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontLeftElbowNdx, 0, map(frontLeftElbowSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontRightElbowNdx, 0, map(frontRightElbowSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backLeftElbowNdx, 0, map(backLeftElbowSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backRightElbowNdx, 0, map(backRightElbowSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   delay(delta);
 
-  servoDriver.setPWM(frontLeftWristNdx, 0, map(frontLeftWristCrouch, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontRightWristNdx, 0, map(frontRightWristCrouch, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backLeftWristNdx, 0, map(backLeftWristCrouch, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backRightWristNdx, 0, map(backRightWristCrouch, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontLeftWristNdx, 0, map(frontLeftWristSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontRightWristNdx, 0, map(frontRightWristSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backLeftWristNdx, 0, map(backLeftWristSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backRightWristNdx, 0, map(backRightWristSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   delay(delta);
 }
 
@@ -90,33 +90,22 @@ void Stand()
   Serial.println("Stand()");
   int deltaT = 200;
 
-  servoDriver.setPWM(frontLeftWristNdx, 0, map(frontLeftWristCrouch - 60, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontRightWristNdx, 0, map(frontRightWristCrouch + 60, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backLeftWristNdx, 0, map(backLeftWristCrouch - 60, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backRightWristNdx, 0, map(backRightWristCrouch + 60, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontLeftWristNdx, 0, map(frontLeftWristSphinx - 60, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontRightWristNdx, 0, map(frontRightWristSphinx + 60, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backLeftWristNdx, 0, map(backLeftWristSphinx - 60, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backRightWristNdx, 0, map(backRightWristSphinx + 60, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   delay(deltaT);
-
-  /*servoDriver.setPWM(backLeftShoulderNdx, 0, map(backLeftShoulderCrouch - 10, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backRightShoulderNdx, 0, map(backRightShoulderCrouch + 10, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  delay(deltaT);*/
 
   for (int deltaTheta = 0; deltaTheta <= 60; deltaTheta += 3)
   {
     
-    servoDriver.setPWM(frontLeftElbowNdx, 0, map(frontLeftElbowCrouch + deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-    servoDriver.setPWM(frontRightElbowNdx, 0, map(frontRightElbowCrouch - deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+    servoDriver.setPWM(frontLeftElbowNdx, 0, map(frontLeftElbowSphinx + deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+    servoDriver.setPWM(frontRightElbowNdx, 0, map(frontRightElbowSphinx - deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   
-    servoDriver.setPWM(backLeftElbowNdx, 0, map(backLeftElbowCrouch + deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-    servoDriver.setPWM(backRightElbowNdx, 0, map(backRightElbowCrouch - deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+    servoDriver.setPWM(backLeftElbowNdx, 0, map(backLeftElbowSphinx + deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+    servoDriver.setPWM(backRightElbowNdx, 0, map(backRightElbowSphinx - deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
     delay(deltaT);
   }
-  /*for (int deltaTheta = 45; deltaTheta <= 75; deltaTheta += 3)
-  {
-    
-    servoDriver.setPWM(frontLeftElbowNdx, 0, map(frontLeftElbowCrouch + deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-    servoDriver.setPWM(frontRightElbowNdx, 0, map(frontRightElbowCrouch - deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-    delay(deltaT);
-  }*/
 }
 
 void StretchWrists()
@@ -125,14 +114,14 @@ void StretchWrists()
   int deltaT = 100;
   for (int deltaTheta = -60; deltaTheta <= 0; deltaTheta += 3)
   {
-    servoDriver.setPWM(backLeftWristNdx, 0, map(backLeftWristCrouch + deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-    servoDriver.setPWM(backRightWristNdx, 0, map(backRightWristCrouch - deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+    servoDriver.setPWM(backLeftWristNdx, 0, map(backLeftWristSphinx + deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+    servoDriver.setPWM(backRightWristNdx, 0, map(backRightWristSphinx - deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
     delay(deltaT);
   }
   for (int deltaTheta = -60; deltaTheta <= 0; deltaTheta += 3)
   {
-    servoDriver.setPWM(frontLeftWristNdx, 0, map(frontLeftWristCrouch + deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-    servoDriver.setPWM(frontRightWristNdx, 0, map(frontRightWristCrouch - deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+    servoDriver.setPWM(frontLeftWristNdx, 0, map(frontLeftWristSphinx + deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+    servoDriver.setPWM(frontRightWristNdx, 0, map(frontRightWristSphinx - deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
     delay(deltaT);
   }
 }
@@ -142,69 +131,69 @@ void Step(int balanceDeltaTheta)
   Serial.println("Step()");
   int deltaT = 200;
   // Balance to the right
-  servoDriver.setPWM(frontLeftShoulderNdx, 0, map(frontLeftShoulderCrouch - balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontRightShoulderNdx, 0, map(frontRightShoulderCrouch - balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backLeftShoulderNdx, 0, map(backLeftShoulderCrouch + balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backRightShoulderNdx, 0, map(backRightShoulderCrouch + balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontLeftShoulderNdx, 0, map(frontLeftShoulderSphinx - balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontRightShoulderNdx, 0, map(frontRightShoulderSphinx - balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backLeftShoulderNdx, 0, map(backLeftShoulderSphinx + balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backRightShoulderNdx, 0, map(backRightShoulderSphinx + balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   delay(deltaT);
   // Extend left wrists
-  servoDriver.setPWM(frontLeftWristNdx, 0, map(frontLeftWristCrouch - 15, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backLeftWristNdx, 0, map(backLeftWristCrouch - 15, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontLeftWristNdx, 0, map(frontLeftWristSphinx - 15, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backLeftWristNdx, 0, map(backLeftWristSphinx - 15, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   delay(deltaT);
   // Retract back left wrist and lift back left leg
-  servoDriver.setPWM(backLeftWristNdx, 0, map(backLeftWristCrouch - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backLeftElbowNdx, 0, map(backLeftElbowCrouch - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backLeftWristNdx, 0, map(backLeftWristSphinx - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backLeftElbowNdx, 0, map(backLeftElbowSphinx - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   delay(deltaT);
   // Put back left tip on the ground
-  servoDriver.setPWM(backLeftWristNdx, 0, map(backLeftWristCrouch + 90, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backLeftElbowNdx, 0, map(backLeftElbowCrouch - 10, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backLeftWristNdx, 0, map(backLeftWristSphinx + 90, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backLeftElbowNdx, 0, map(backLeftElbowSphinx - 10, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   delay(2 * deltaT);
 
   // Retract front left wrist and lift back left leg
-  servoDriver.setPWM(frontLeftWristNdx, 0, map(frontLeftWristCrouch - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontLeftElbowNdx, 0, map(frontLeftElbowCrouch - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontLeftWristNdx, 0, map(frontLeftWristSphinx - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontLeftElbowNdx, 0, map(frontLeftElbowSphinx - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   delay(deltaT);
   // Put front left tip on the ground
-  servoDriver.setPWM(frontLeftWristNdx, 0, map(frontLeftWristCrouch + 90, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontLeftElbowNdx, 0, map(frontLeftElbowCrouch - 10, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontLeftWristNdx, 0, map(frontLeftWristSphinx + 90, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontLeftElbowNdx, 0, map(frontLeftElbowSphinx - 10, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   delay(deltaT);
 
   // Raise left legs
-  servoDriver.setPWM(backLeftElbowNdx, 0, map(backLeftElbowCrouch, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontLeftElbowNdx, 0, map(frontLeftElbowCrouch, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backLeftElbowNdx, 0, map(backLeftElbowSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontLeftElbowNdx, 0, map(frontLeftElbowSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   delay(deltaT);
 
   // Balance to the center
-  /*servoDriver.setPWM(frontLeftShoulderNdx, 0, map(frontLeftShoulderCrouch + balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontRightShoulderNdx, 0, map(frontRightShoulderCrouch + balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backLeftShoulderNdx, 0, map(backLeftShoulderCrouch - balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backRightShoulderNdx, 0, map(backRightShoulderCrouch - balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  /*servoDriver.setPWM(frontLeftShoulderNdx, 0, map(frontLeftShoulderSphinx + balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontRightShoulderNdx, 0, map(frontRightShoulderSphinx + balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backLeftShoulderNdx, 0, map(backLeftShoulderSphinx - balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backRightShoulderNdx, 0, map(backRightShoulderSphinx - balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   delay(deltaT);*/
 
   // Extend back right wrist
-  servoDriver.setPWM(backRightElbowNdx, 0, map(backRightElbowCrouch - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backRightWristNdx, 0, map(backRightWristCrouch - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontRightElbowNdx, 0, map(frontRightElbowCrouch - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontRightWristNdx, 0, map(frontRightWristCrouch - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backRightElbowNdx, 0, map(backRightElbowSphinx - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backRightWristNdx, 0, map(backRightWristSphinx - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontRightElbowNdx, 0, map(frontRightElbowSphinx - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontRightWristNdx, 0, map(frontRightWristSphinx - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   delay(deltaT);
   // Retract front right wrist and lift front right leg
-  /*servoDriver.setPWM(frontRightWristNdx, 0, map(frontRightWristCrouch + 120, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontRightElbowNdx, 0, map(frontRightElbowCrouch + 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontRightWristNdx, 0, map(frontRightWristCrouch - 10, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  /*servoDriver.setPWM(frontRightWristNdx, 0, map(frontRightWristSphinx + 120, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontRightElbowNdx, 0, map(frontRightElbowSphinx + 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontRightWristNdx, 0, map(frontRightWristSphinx - 10, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   delay(deltaT);*/
   
   
   /*// Put back left tip on the ground
-  servoDriver.setPWM(backRightWristNdx, 0, map(backRightWristCrouch + 90, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backRightElbowNdx, 0, map(backRightElbowCrouch - 10, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backRightWristNdx, 0, map(backRightWristSphinx + 90, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backRightElbowNdx, 0, map(backRightElbowSphinx - 10, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   delay(deltaT);*/
-  /*servoDriver.setPWM(frontLeftWristNdx, 0, map(frontLeftWristCrouch - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  /*servoDriver.setPWM(frontLeftWristNdx, 0, map(frontLeftWristSphinx - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   delay(deltaT);
-  servoDriver.setPWM(frontLeftElbowNdx, 0, map(frontLeftElbowCrouch, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontLeftElbowNdx, 0, map(frontLeftElbowSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   delay(deltaT);*/
-  /*servoDriver.setPWM(backLeftShoulderNdx, 0, map(backLeftShoulderCrouch, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  /*servoDriver.setPWM(backLeftShoulderNdx, 0, map(backLeftShoulderSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   delay(delta);
-  servoDriver.setPWM(backRightShoulderNdx, 0, map(backRightShoulderCrouch, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backRightShoulderNdx, 0, map(backRightShoulderSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   delay(delta);*/
 
   // Restore center balance
