b05ba5431eb51f41177ff64e7e17a94877678d45
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2021-03-13 12:21:23 -0500

    legsControl.ino: Added SimultaneousMoves()

--- legsControl/legsControl.ino
+++ legsControl/legsControl.ino
@@ -1,5 +1,8 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
+#include <ArxContainer.h>
+
+using namespace std;
 
 #define MG996RSERVOMIN 150
 #define MG996RSERVOMAX 650
@@ -23,15 +26,38 @@ uint8_t backLeftWristNdx = 14;
 int frontLeftShoulderSphinx = 85;
 int frontRightShoulderSphinx = 110;
 int frontLeftElbowSphinx = 0;
-int frontRightElbowSphinx = 140;
-int frontLeftWristSphinx = 80;
+int frontRightElbowSphinx = 120;
+int frontLeftWristSphinx = 90;
 int frontRightWristSphinx = 130;
 int backLeftShoulderSphinx = 110;
 int backRightShoulderSphinx = 105;
-int backLeftElbowSphinx = 90;
+int backLeftElbowSphinx = 95;
 int backRightElbowSphinx = 120;
 int backLeftWristSphinx = 80;
 int backRightWristSphinx = 130;
+
+int frontLeftWristStand = frontLeftWristSphinx - 60;
+int frontRightWristStand = frontRightWristSphinx + 60;
+int backLeftWristStand = backLeftWristSphinx - 60;
+int backRightWristStand = backRightWristSphinx + 60;
+int frontLeftElbowStand = frontLeftElbowSphinx + 60;
+int frontRightElbowStand = frontRightElbowSphinx - 60;
+int backLeftElbowStand = backLeftElbowSphinx + 60;
+int backRightElbowStand = backRightElbowSphinx - 60;
+
+int frontLeftElbowLift = frontLeftElbowStand - 90;
+int frontRightElbowLift = frontRightElbowStand + 90;
+int backLeftElbowLift = backLeftElbowStand - 90;
+int backRightElbowLift = backRightElbowStand + 90;
+
+int frontLeftElbowLanding = frontLeftElbowLift + 30;
+int frontRightElbowLanding = frontRightElbowLift -30;
+int backLeftElbowLanding = backLeftElbowLift + 30;
+int backRightElbowLanding = backRightElbowLift - 30;
+int frontLeftWristLanding = frontLeftWristSphinx + 110;
+int frontRightWristLanding = frontRightWristSphinx + 0;
+int backLeftWristLanding = backLeftWristSphinx + 0;
+int backRightWristLanding = backRightWristSphinx + 0;
 
 
 Adafruit_PWMServoDriver servoDriver = Adafruit_PWMServoDriver();
@@ -50,9 +76,8 @@ void setup()
   delay(2000);
   Stand();
   delay(2000);
-  //StretchWrists();
-  //delay(2000);
-  //Step(10);
+
+  Step(10);
 }
 
 void loop()
@@ -90,22 +115,18 @@ void Stand()
   Serial.println("Stand()");
   int deltaT = 200;
 
-  servoDriver.setPWM(frontLeftWristNdx, 0, map(frontLeftWristSphinx - 60, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontRightWristNdx, 0, map(frontRightWristSphinx + 60, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backLeftWristNdx, 0, map(backLeftWristSphinx - 60, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backRightWristNdx, 0, map(backRightWristSphinx + 60, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontLeftWristNdx, 0, map(frontLeftWristStand, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(frontRightWristNdx, 0, map(frontRightWristStand, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backLeftWristNdx, 0, map(backLeftWristStand, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  servoDriver.setPWM(backRightWristNdx, 0, map(backRightWristStand, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
   delay(deltaT);
 
-  for (int deltaTheta = 0; deltaTheta <= 60; deltaTheta += 3)
-  {
-    
-    servoDriver.setPWM(frontLeftElbowNdx, 0, map(frontLeftElbowSphinx + deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-    servoDriver.setPWM(frontRightElbowNdx, 0, map(frontRightElbowSphinx - deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  
-    servoDriver.setPWM(backLeftElbowNdx, 0, map(backLeftElbowSphinx + deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-    servoDriver.setPWM(backRightElbowNdx, 0, map(backRightElbowSphinx - deltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-    delay(deltaT);
-  }
+  arx::vector<arx::vector<int> > jointNdxStarStopVct;
+  jointNdxStarStopVct.push_back(arx::vector<int> {frontLeftElbowNdx, frontLeftElbowSphinx, frontLeftElbowStand});
+  jointNdxStarStopVct.push_back(arx::vector<int> {frontRightElbowNdx, frontRightElbowSphinx, frontRightElbowStand});
+  jointNdxStarStopVct.push_back(arx::vector<int> {backLeftElbowNdx, backLeftElbowSphinx, backLeftElbowStand});
+  jointNdxStarStopVct.push_back(arx::vector<int> {backRightElbowNdx, backRightElbowSphinx, backRightElbowStand});
+  SimultaneousMoves(jointNdxStarStopVct, 6, 100);
 }
 
 void StretchWrists()
@@ -130,71 +151,68 @@ void Step(int balanceDeltaTheta)
 {
   Serial.println("Step()");
   int deltaT = 200;
-  // Balance to the right
-  servoDriver.setPWM(frontLeftShoulderNdx, 0, map(frontLeftShoulderSphinx - balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontRightShoulderNdx, 0, map(frontRightShoulderSphinx - balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backLeftShoulderNdx, 0, map(backLeftShoulderSphinx + balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backRightShoulderNdx, 0, map(backRightShoulderSphinx + balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  delay(deltaT);
-  // Extend left wrists
-  servoDriver.setPWM(frontLeftWristNdx, 0, map(frontLeftWristSphinx - 15, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backLeftWristNdx, 0, map(backLeftWristSphinx - 15, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  delay(deltaT);
-  // Retract back left wrist and lift back left leg
-  servoDriver.setPWM(backLeftWristNdx, 0, map(backLeftWristSphinx - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backLeftElbowNdx, 0, map(backLeftElbowSphinx - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  delay(deltaT);
-  // Put back left tip on the ground
-  servoDriver.setPWM(backLeftWristNdx, 0, map(backLeftWristSphinx + 90, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backLeftElbowNdx, 0, map(backLeftElbowSphinx - 10, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  delay(2 * deltaT);
-
-  // Retract front left wrist and lift back left leg
-  servoDriver.setPWM(frontLeftWristNdx, 0, map(frontLeftWristSphinx - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontLeftElbowNdx, 0, map(frontLeftElbowSphinx - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  delay(deltaT);
-  // Put front left tip on the ground
-  servoDriver.setPWM(frontLeftWristNdx, 0, map(frontLeftWristSphinx + 90, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontLeftElbowNdx, 0, map(frontLeftElbowSphinx - 10, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  delay(deltaT);
-
-  // Raise left legs
-  servoDriver.setPWM(backLeftElbowNdx, 0, map(backLeftElbowSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontLeftElbowNdx, 0, map(frontLeftElbowSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  delay(deltaT);
-
-  // Balance to the center
-  /*servoDriver.setPWM(frontLeftShoulderNdx, 0, map(frontLeftShoulderSphinx + balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontRightShoulderNdx, 0, map(frontRightShoulderSphinx + balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backLeftShoulderNdx, 0, map(backLeftShoulderSphinx - balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backRightShoulderNdx, 0, map(backRightShoulderSphinx - balanceDeltaTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  delay(deltaT);*/
-
-  // Extend back right wrist
-  servoDriver.setPWM(backRightElbowNdx, 0, map(backRightElbowSphinx - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backRightWristNdx, 0, map(backRightWristSphinx - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontRightElbowNdx, 0, map(frontRightElbowSphinx - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontRightWristNdx, 0, map(frontRightWristSphinx - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  delay(deltaT);
-  // Retract front right wrist and lift front right leg
-  /*servoDriver.setPWM(frontRightWristNdx, 0, map(frontRightWristSphinx + 120, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontRightElbowNdx, 0, map(frontRightElbowSphinx + 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(frontRightWristNdx, 0, map(frontRightWristSphinx - 10, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  delay(deltaT);*/
+  // Balance to the right and lift back left leg
+  arx::vector<arx::vector<int> > jointNdxStarStopVct;
+  jointNdxStarStopVct.push_back(arx::vector<int> {frontLeftShoulderNdx, frontLeftShoulderSphinx, frontLeftShoulderSphinx - balanceDeltaTheta});
+  jointNdxStarStopVct.push_back(arx::vector<int> {frontRightShoulderNdx, frontRightShoulderSphinx, frontRightShoulderSphinx - balanceDeltaTheta});
+  jointNdxStarStopVct.push_back(arx::vector<int> {backLeftShoulderNdx, backLeftShoulderSphinx, backLeftShoulderSphinx + balanceDeltaTheta});
+  jointNdxStarStopVct.push_back(arx::vector<int> {backRightShoulderNdx, backRightShoulderSphinx, backRightShoulderSphinx + balanceDeltaTheta});
+  jointNdxStarStopVct.push_back(arx::vector<int> {backLeftElbowNdx, backLeftElbowStand, backLeftElbowLift});
+  SimultaneousMoves(jointNdxStarStopVct, 6, 100);
+  servoDriver.setPWM(backLeftWristNdx, 0, map(backLeftWristSphinx + 110, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+  jointNdxStarStopVct.clear();
   
-  
-  /*// Put back left tip on the ground
-  servoDriver.setPWM(backRightWristNdx, 0, map(backRightWristSphinx + 90, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  servoDriver.setPWM(backRightElbowNdx, 0, map(backRightElbowSphinx - 10, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  delay(deltaT);*/
-  /*servoDriver.setPWM(frontLeftWristNdx, 0, map(frontLeftWristSphinx - 30, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  delay(deltaT);
-  servoDriver.setPWM(frontLeftElbowNdx, 0, map(frontLeftElbowSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  delay(deltaT);*/
-  /*servoDriver.setPWM(backLeftShoulderNdx, 0, map(backLeftShoulderSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  delay(delta);
-  servoDriver.setPWM(backRightShoulderNdx, 0, map(backRightShoulderSphinx, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
-  delay(delta);*/
+}
 
-  // Restore center balance
+void ElbowAndWrist(uint8_t elbowNdx, uint8_t wristNdx, int elbowTheta1, int elbowTheta2, int wristTheta1, int wristTheta2, int numberOfSteps, int deltaT)
+{
+  for (int stepNdx = 1; stepNdx <= numberOfSteps; stepNdx++)
+  {
+    int elbowTheta = elbowTheta1 + stepNdx * (elbowTheta2 - elbowTheta1)/numberOfSteps;
+    int wristTheta = wristTheta1 + stepNdx * (wristTheta2 - wristTheta1)/numberOfSteps;
+    servoDriver.setPWM(elbowNdx, 0, map(elbowTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+    servoDriver.setPWM(wristNdx, 0, map(wristTheta, 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+    delay(deltaT);
+  }
+}
+
+void SimultaneousMoves(arx::vector<arx::vector<int> > jointNdxStartStopVct, int numberOfSteps, int deltaT)
+{
+  for (int stepNdx = 1; stepNdx <= numberOfSteps; stepNdx++)
+  {
+    double alpha = (double) stepNdx / numberOfSteps;
+    for (size_t jointNdxNdx = 0; jointNdxNdx < jointNdxStartStopVct.size(); jointNdxNdx++)
+    {
+      arx::vector<int> jointNdxStartStop = jointNdxStartStopVct[jointNdxNdx];
+      int jointNdx = jointNdxStartStop[0];
+      int startTheta = jointNdxStartStop[1];
+      int stopTheta = jointNdxStartStop[2];
+      servoDriver.setPWM(jointNdx, 0, map(startTheta + alpha * (stopTheta - startTheta), 0, 180, MG996RSERVOMIN, MG996RSERVOMAX));
+    }
+    delay(deltaT);
+  }
+}
+
+void Prayer()
+{
+  int numberOfSteps = 8;
+  int deltaT = 100;
+  ElbowAndWrist(frontLeftElbowNdx, frontLeftWristNdx, frontLeftElbowSphinx, frontLeftElbowSphinx - 50, frontLeftWristSphinx, frontLeftWristSphinx + 80,
+  numberOfSteps, deltaT);
+  ElbowAndWrist(frontRightElbowNdx, frontRightWristNdx, frontRightElbowSphinx, frontRightElbowSphinx + 50, frontRightWristSphinx, frontRightWristSphinx - 80,
+  numberOfSteps, deltaT);
+}
+
+void HoovesOnGround()
+{
+  arx::vector<arx::vector<int> > jointNdxStarStopVct;
+  jointNdxStarStopVct.push_back(arx::vector<int> {frontLeftWristNdx, frontLeftWristSphinx, frontLeftWristSphinx + 100});
+  jointNdxStarStopVct.push_back(arx::vector<int> {frontLeftElbowNdx, frontLeftElbowSphinx, frontLeftElbowSphinx - 50});
+  jointNdxStarStopVct.push_back(arx::vector<int> {frontRightWristNdx, frontRightWristSphinx, frontRightWristSphinx - 100});
+  jointNdxStarStopVct.push_back(arx::vector<int> {frontRightElbowNdx, frontRightElbowSphinx, frontRightElbowSphinx + 50});
+  /*jointNdxStarStopVct.push_back(arx::vector<int> {backLeftWristNdx, backLeftWristSphinx, backLeftWristSphinx + 120});
+  jointNdxStarStopVct.push_back(arx::vector<int> {backLeftElbowNdx, backLeftElbowSphinx, backLeftElbowSphinx - 30});
+  jointNdxStarStopVct.push_back(arx::vector<int> {backRightWristNdx, backRightWristSphinx, backRightWristSphinx - 100});
+  jointNdxStarStopVct.push_back(arx::vector<int> {backRightElbowNdx, backRightElbowSphinx, backRightElbowSphinx + 50});*/
+  SimultaneousMoves(jointNdxStarStopVct, 8, 200);
 }
